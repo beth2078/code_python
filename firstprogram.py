b8f686ed7f29f8fa877b4e611322b23db927ab3d
beth2078 <babowden2016@outlook.com>
2024-09-19 09:34:08 -0400

    Basic Calculator

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+#Basic calulator
+#enter "python firstprogram.py" to excecute code
+print("Welcome to the calculator!")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2 
+print(f"The answer is: {answer}")
