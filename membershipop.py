e1300057763168690610e3ea34b1012065544776
beth2078 <babowden2016@outlook.com>
2024-09-26 19:19:47 -0400

    Membership Operations & Exploring Boolean

---
+++ membershipop.py
@@ -0,0 +1,16 @@
+#Exploring Boolean & Membershop Operations
+
+# name = input("Enter your name: ")
+# if not name: #a way to chack if a variable has an empty value
+#     print("Name was not valid.")
+# else:
+#     print(f"Hello {name}!")
+
+if "a" in "Bethany":
+    print("'a' is found in Bethany")
+
+if "Apple" not in ["Kiwi", "Blueberry", "Watermelon"]:
+    print("Apple was not found in out list of fruits")
+
+if "water" in "watermelon":
+    print("water was found in watermelon")
